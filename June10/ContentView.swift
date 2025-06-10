6238e4c760ee25ad79c7b6eb43110fb9652c8d35
Vikas Raj Yadav <vikasyadav.vision111@gmail.com>
2025-06-10 14:30:53 +0530

    Second commit

--- June10/ContentView.swift
+++ June10/ContentView.swift
@@ -8,15 +8,38 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var speed: Double = 50
+    @State private var isEditing = false
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack(spacing: 24) {
+            
+            Text("\(Int(speed))")
+                .font(.system(size: 180))
+                .fontDesign(.monospaced)
+                .fontWeight(.bold)
+                .contentTransition(.numericText(value: speed))
+                .blur(radius: isEditing ? 2 : 0)
+                .scaleEffect(isEditing ? 1.1 : 1.0)
+                .animation(.easeInOut(duration: 0.3), value: isEditing)
+                .animation(.easeInOut(duration: 0.5), value: speed)
+           
+            Slider( value: $speed, in: 0...100, step: 1,
+                onEditingChanged: { editing in
+                    withAnimation(.easeInOut(duration: 0.3)) {
+                        isEditing = editing
+                    }
+                }
+            )
+            .padding(.horizontal)
+            .frame(width: 220)
+            .opacity(isEditing ? 1 : 0.8)
+            .animation(.easeInOut(duration: 0.3), value: isEditing)
         }
-        .padding()
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(Color(red: 203/255, green: 203/255, blue: 205/255))
     }
+       
 }
 
 #Preview {
