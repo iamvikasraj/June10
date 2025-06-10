e748600f6048f4e70d6b337bf47bb2466ee14000
Vikas Raj Yadav <vikasyadav.vision111@gmail.com>
2025-06-10 09:48:21 +0530

    Initial Commit

---
+++ June10/June10App.swift
@@ -0,0 +1,17 @@
+//
+//  June10App.swift
+//  June10
+//
+//  Created by Vikas Raj Yadav on 10/06/25.
+//
+
+import SwiftUI
+
+@main
+struct June10App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ June10Tests/June10Tests.swift
@@ -0,0 +1,16 @@
+//
+//  June10Tests.swift
+//  June10Tests
+//
+//  Created by Vikas Raj Yadav on 10/06/25.
+//
+
+import Testing
+
+struct June10Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
